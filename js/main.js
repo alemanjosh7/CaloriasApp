0354eadc090c09687fd3b1e5cd19d73ce860a3d7
alemanjosh7 <josuealeman2003@gmail.com>
2021-06-22 01:00:52 -0600

    Finishing html and css base

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-//main
+const themeButton = document.getElementById("theme-button");
+const navbar = document.getElementById("navbar");
+const logo = document.getElementById("logo");
+
+themeButton.addEventListener("click", event => {
+    if (themeButton.src.match("light-solid")) {
+        themeButton.src = "../resources/img/light.png";
+        document.body.style.background = "#fff";
+        navbar.style.background = "#14274E";
+        logo.style.color = "#fff";
+    }
+    else {
+        themeButton.src = "../resources/img/light-solid.png";
+        document.body.style.background = "#fff";
+        navbar.style.background = "#4A69BB";
+    }
+});
